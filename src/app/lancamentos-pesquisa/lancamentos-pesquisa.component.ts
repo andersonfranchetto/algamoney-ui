2a8fa427adaf334be0f04742621c3358dbe4272b
Anderson <anderson.franchetto@gmail.com>
2018-09-24 22:14:46 -0300

    10.12. Adicionando menu intercambiável

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -10,6 +10,7 @@ export class LancamentosPesquisaComponent {
   lancamentos = [
     {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/07/2017', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'},
     {tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: '10/06/2017', dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'}
+    {tipo: 'DESPESA', descricao: 'Pagamento de luz', dataVencimento: '10/06/2017', dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Elktro SA'}
   ]
 
 }
