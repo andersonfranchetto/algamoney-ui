b2f5e1b34e79e16661cc74e11d8c123c36e8efe5
Anderson <anderson.franchetto@gmail.com>
2018-09-24 22:36:35 -0300

    10.13. Desafio: criando componente de pesquisa de pessoas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { ButtonModule } from 'primeng/components/button/button';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+
 import { NavbarComponent } from './navbar/navbar.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     LancamentosPesquisaComponent,
-    NavbarComponent
+    PessoasPesquisaComponent
   ],
   imports: [
     BrowserModule,
