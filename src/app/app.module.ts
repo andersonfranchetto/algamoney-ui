6256067620142b4973e0cf705d74c723556d614a
Anderson <anderson.franchetto@gmail.com>
2018-09-25 15:38:47 -0300

    13.1. Criando o protótipo do formulário de cadastro de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,20 +10,24 @@ import { TooltipModule } from 'primeng/tooltip';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import {InputTextareaModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LancamentosPesquisaComponent,
-    PessoasPesquisaComponent
+    PessoasPesquisaComponent,
+    LancamentoCadastroComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
     ButtonModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
